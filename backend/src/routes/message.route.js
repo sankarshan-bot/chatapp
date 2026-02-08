d6442680d84dd5b56323436349b9fbeb57b0c1a0
sankarshan <sankarshanprabhu1@gmail.com>
2026-02-08 18:39:25 +0530

    project done

---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { getMessages, getUserForSidebar, sendMessage } from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+
+
+const router=express.Router();
+
+router.get("/users",protectRoute,getUserForSidebar)
+router.get("/:id",protectRoute,getMessages)
+router.post("/send/:id",protectRoute,sendMessage)
+
+
+export default router;
